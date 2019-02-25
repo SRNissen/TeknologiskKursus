dc92d9e7032742245cb2b21efb9e4ab31c886619
SRNissen <nope@nope.com>
2019-02-25 13:18:04 +0100

    Change for the sake of change

--- TestProjekt/Program.cs
+++ TestProjekt/Program.cs
@@ -6,7 +6,7 @@ namespace TestProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world!");
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
