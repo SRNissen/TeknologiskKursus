eaf066dec85cf2b54763c78dd992a7ebd70f6894
SRNissen <nope@nope.com>
2019-03-11 11:27:17 +0100

    More Exceptions

---
+++ ErrorHandling_Exceptions_RollingYourOwn/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Du skal i Program-klassen i en ny konsol-applikationskabe
+ * skabe en statisk SmartToUpper-metode,
+ * som modtager en streng som argument,
+ * og returnerer strengen med små bogstaver
+ * bortset fra det første bogstav som skal være stort.
+ * Altså "mikkel" bliver til "Mikkel"
+ * og "MAtHiAs" bliver til "Mathias".*/
+
+using System;
+
+namespace ErrorHandling_Exceptions_RollingYourOwn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine(SmartToUpper("mikkel"));
+                Console.WriteLine(SmartToUpper("mAtHiAs"));
+                Console.WriteLine(SmartToUpper("a"));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+
+        static string SmartToUpper(string input)
+        {
+            if (input == null)
+            {
+                throw new ApplicationException("Object Reference is not set to an instance of Object");
+            }
+
+            if (input.Length == 0)
+            {
+                throw new ApplicationException("TextMustBeLongerThanZero");
+            }
+
+            string FirstLetter = input.Substring(0, 1).ToUpper();
+            string otherLetters = input.Substring(1).ToLower();
+            string output = FirstLetter + otherLetters;
+            return output;
+        }
+    }
+}
---
+++ ErrorHandling_Exceptions_RollingYourOwn_Advanced/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Du skal skabe en applikation som
+
+Først checke om filen c:\temp\tal.txt eksisterer (System.IO.File.Exists).
+Hvis den ikke findes skal der smides en ApplikationException med teksten "Filen c:\temp\tal.txt findes ikke!"
+I en try/catch-struktur skal du nu
+    Åbne filen og hent indhold til en streng (System.IO.File.ReadAllText).
+        Hvis det går galt skal fejlen blot smides videre.
+    Konverter indhold til et heltal (int32) ved hjælp af Convert.ToInt32.
+        Hvis det går galt smider ToInt32 en FormatException.
+        I så fald skal du smide en ApplikationException med teksten "Indhold kan ikke konverteres til et heltal".
+    Læg nu 1 til tallet og gem det igen (System.IO.File.WriteAllText).
+        Hvis det går galt skal fejlen blot smides videre.
+ */
+
+
+using System;
+
+namespace ErrorHandling_Exceptions_RollingYourOwn_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
