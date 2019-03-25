3bd5a904928e416a585805b8294a68366d02a817
SRNissen <test@test.test>
2019-03-25 15:39:39 +0100

    -_-

---
+++ Delegates_Log/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates_Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Machine m = new Machine { Log = Console.WriteLine };
+            m.Start();
+            System.Threading.Thread.Sleep(1000);
+            m.Stop();
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Machine
+    {
+        public Action<string> Log { get; set; }
+
+        public void Start()
+        {
+            Log($"Start: {System.Convert.ToString(System.DateTime.Now)}");
+        }
+
+        public void Stop()
+        {
+            Log($"Stop: {System.Convert.ToString(System.DateTime.Now)}");
+        }
+    }
+}
