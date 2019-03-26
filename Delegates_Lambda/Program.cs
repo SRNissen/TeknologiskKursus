ae29106e98f15a20a406c84fc74ad79db56d9589
SRNissen <test@test.test>
2019-03-26 10:15:50 +0100

    _-_

--- Delegates_Lambda/Program.cs
+++ Delegates_Lambda/Program.cs
@@ -5,23 +5,28 @@ namespace Delegates_Lambda
 {
     class Program
     {
-        public static bool Find51(int a)
-        {
-            return a == 51;
-        }
-
-        public static void Write(int a)
-        {
-            Console.WriteLine(a);
-        }
 
         static void Main(string[] args)
         {
             List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
 
-            int index = a.FindIndex(Find51);
-            Console.WriteLine(index);
+            bool Find51(int x)
+            {
+                return x == 51;
+            }
+            void Write(int x)
+            {
+                Console.WriteLine(x);
+            }
+            int index0 = a.FindIndex(Find51);
+            Console.WriteLine(index0);
             a.ForEach(Write);
+
+            Console.WriteLine("------------------------------");
+
+            int index1 = a.FindIndex((int x) => { return x == 51; });
+            Console.WriteLine(index1);
+            a.ForEach((int x) => { Console.WriteLine(x); });
 
             Console.WriteLine("------------------------------");
 
