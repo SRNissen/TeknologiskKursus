9f1561c4da318f0e89e6f43c4ddcac3c31ba5276
SRNissen <test@test.test>
2019-03-26 09:42:01 +0100

    -_-;;;;

--- Delegates_Lambda/Program.cs
+++ Delegates_Lambda/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegates_Lambda
 {
     class Program
     {
+        public static bool Find51(int a)
+        {
+            return a == 51;
+        }
+
+        public static void Write(int a)
+        {
+            Console.WriteLine(a);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
+
+            int index = a.FindIndex(Find51);
+            Console.WriteLine(index);
+            a.ForEach(Write);
+
+            Console.WriteLine("------------------------------");
+
+            int index2 = a.FindIndex(x => x == 51);
+            Console.WriteLine(index2);
+            a.ForEach(x => Console.WriteLine(x));
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
         }
     }
 }
